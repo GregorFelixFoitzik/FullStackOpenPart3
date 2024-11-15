c656c21d480ac8baadd070af2f18a4c968a149aa
Gregor Foitzik <greg.foitzik@gmail.com>
2024-11-15 13:33:11 +0200

    part3 exercise 22

--- models/person.js
+++ models/person.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -12,32 +12,32 @@ mongoose.connect(url)
 
 
 
-  const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minLength: 3,
-    },
-    number: {
-      type: String,
-      required: true,
-      minLength: 8,
-      validate: {
-        validator: function(v) {
-            return /^\d{2,3}-\d+$/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number!`
-      }
-    },
-    id: Number,
-  })
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+  id: Number,
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 
